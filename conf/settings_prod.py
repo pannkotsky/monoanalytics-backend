dcaa6866dfa96e60bfe34d72bad9ec9b8442e815
Valerii Kovalchuk <kovvalole@gmail.com>
2024-11-08 23:31:47 +0200

    Update CORS allowed origins

--- conf/settings_prod.py
+++ conf/settings_prod.py
@@ -7,8 +7,10 @@ DATABASES["default"]["CONN_MAX_AGE"] = 600  # noqa: F405
 DATABASES["default"]["CONN_HEALTH_CHECKS"] = True  # noqa: F405
 DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}  # noqa: F405
 
-CORS_ALLOWED_ORIGINS = []
-CORS_ALLOWED_ORIGIN_REGEXES = [r"https://.*\.monoanalytics\.app$"]
+CORS_ALLOWED_ORIGINS = [
+    "https://monoanalytics-ui.vercel.app",
+    "https://monoanalytics.app",
+]
 
 SECURE_HSTS_SECONDS = 3600
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
