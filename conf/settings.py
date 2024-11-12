98a002b306f7432b2ec2b6c9e3e597fa10764d88
Valerii Kovalchuk <kovvalole@gmail.com>
2024-11-12 13:56:16 +0200

    Configure request extended logging

--- conf/settings.py
+++ conf/settings.py
@@ -88,6 +88,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "request_logging.middleware.LoggingMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
